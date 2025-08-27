de143c2404e5f1a93c74ae5d2084958b2c7a443a
Søren Pedersen <sh.pedersen@gmail.com>
2025-08-27 11:35:55 +0200

    Add new UseNullPrincipalProvider

--- src/Likvido.Identity/DependencyInjection.cs
+++ src/Likvido.Identity/DependencyInjection.cs
@@ -6,6 +6,11 @@ namespace Likvido.Identity
 {
     public static class DependencyInjection
     {
+        public static void UseNullPrincipalProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IPrincipalProvider, NullPrincipalProvider>();
+        }
+
         public static void TryAddNullPrincipalProvider(this IServiceCollection services)
         {
             services.TryAddSingleton<IPrincipalProvider, NullPrincipalProvider>();
